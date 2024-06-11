390118b26f8bb32c3363c636340cd1cc2996444e
Artem <zinykov_a@mail.ru>
2024-06-11 22:32:58 +0300

    Добавил проект

---
+++ scripts.py
@@ -0,0 +1,31 @@
+import glob
+import os
+import random
+import requests
+
+
+def download_comics_xkcd():
+    random_comics = random.randint(1, 2944)  # Выбор из всех комиксов, где 2944 последний комикс
+    url = f"https://xkcd.com/{random_comics}/info.0.json"
+    response = requests.get(url)
+    response.raise_for_status()
+    response_json = response.json()
+    url_image = response_json["img"]
+    comic_number = response_json["num"]
+    filename = f"Images/{comic_number}.png"
+    write_to_file(url_image, filename)
+
+
+def write_to_file(url, filename, params=None):
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    with open(filename, "wb") as file:
+        file.write(response.content)
+
+
+def remove_file():
+    document_dir = "./Images"
+    files = glob.glob(os.path.join(document_dir, '*'))
+    for file in files:
+        os.remove(file)
+
---
+++ tgbot.py
@@ -0,0 +1,30 @@
+import os
+
+import telegram
+
+from environs import Env
+from scripts import download_comics_xkcd, remove_file
+
+
+def send_messages(bot, chat_id):
+    document_dir = os.listdir("./Images")
+    for comics in document_dir:
+        path_to_file = f"./images/{comics}"
+        with open(path_to_file, "rb") as file:
+            document = file.read()
+        bot.send_document(chat_id=chat_id,
+                          document=document)
+
+
+def main():
+    env = Env()
+    env.read_env()
+    bot = telegram.Bot(token=env.str("TG_BOT_TOKEN"))
+    tg_chat_id = env.str("TG_CHAT_ID")
+    download_comics_xkcd()
+    send_messages(bot, tg_chat_id)
+    remove_file()
+
+
+if __name__ == "__main__":
+    main()
